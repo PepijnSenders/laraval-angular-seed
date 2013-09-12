8c73f4d3d913ab804a7d25638985fbeabbda77e0
Pepijn Senders <p.senders@live.nl>
2013-09-12 15:38:40 +0200

    environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,8 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+    'development' => array('*.dev'),
+    'staging' => array('staging.*'),
 
 ));
 
